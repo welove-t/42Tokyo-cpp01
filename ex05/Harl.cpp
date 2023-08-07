b766be39863ce067ca677e2bc17faefc730260e7
love <tlove.tky@gmail.com>
2023-08-07 10:32:24 +0900

    ex05: 出力内容を追加

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,1 +1,35 @@
 #include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+	std::cout << GREEN
+			<< "I love having extra bacon for "
+			<< "my 7XL-double-cheese-triple-pickle-special- ketchup burger. "
+			<< "I really do!"
+			<< RESET << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << BLUE
+				<< "I cannot believe adding extra bacon costs more money. "
+				<< "You didn’t put enough bacon in my burger! If you did, "
+				<< "I wouldn’t be asking for more!"
+				<< RESET << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << YELLOW
+			<< "I think I deserve to have some extra bacon for free."
+			<< "I’ve been coming for years whereas you started working here"
+			<< "since last month."
+			<< RESET << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << RED
+			<< "This is unacceptable! I want to speak to the manager now."
+			<< RESET << std::endl;
+}
