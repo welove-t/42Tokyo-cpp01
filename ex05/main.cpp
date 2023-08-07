a222a1509b08de301f99fd15ca1d75f3699c7b3e
love <tlove.tky@gmail.com>
2023-08-07 12:23:59 +0900

    ex05: main実装

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -2,7 +2,24 @@
 
 int main()
 {
+	std::string input = "";
+	Harl harl;
 
+	std::cout << "Please Enter A Level ( DEBUG | INFO | WARNING | ERROR)" << std::endl;
+	do
+	{
+		std::cout << " > " << std::flush;
+		std::getline(std::cin, input);
+		if (std::cin.eof() || !input.compare("EXIT"))
+			break;
+		if (input.empty())
+			continue;
+		harl.complain(input);
+
+	} while (true);
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cout << "Thank You !" << std::endl;
 	return 0;
 }
 
