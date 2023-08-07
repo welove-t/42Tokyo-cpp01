a5b16cdd5ca8379f130c2e79a0765779e783ec1d
love <tlove.tky@gmail.com>
2023-08-07 12:51:29 +0900

    ex06: first commit

---
+++ ex06/Harl.hpp
@@ -0,0 +1,30 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+
+#define RED		"\e[0;31m" // Red
+#define GREEN	"\e[0;32m" // Green
+#define YELLOW	"\e[0;33m" // Yellow
+#define BLUE	"\e[0;34m" // Blue
+#define RESET 	"\e[0m"    // Reset
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+		enum class Level
+		{
+			Debug,
+			Info,
+			Warning,
+			Error,
+		};
+	public:
+		void	complain(std::string level);
+};
+
+#endif
---
+++ ex06/main.cpp
@@ -0,0 +1,20 @@
+#include "Harl.hpp"
+
+int main(int argc, char *argv[])
+{
+	Harl harl;
+	if (argc != 2)
+	{
+		std::cerr << RED
+					<< "Usage: " << argv[0]
+					<< " < DEBUG | INFO | WARNING | ERROR >"
+					<< RESET << std::endl;
+	}
+	harl.complain(argv[1]);
+	return 0;
+}
+
+__attribute((destructor))
+static void destructor() {
+	system("leaks -q harlFilter");
+}
